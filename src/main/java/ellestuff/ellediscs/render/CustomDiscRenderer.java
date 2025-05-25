e99e99518768742d579fa404205ce6fef66e1962
ellestuff. <ellef1566@gmail.com>
2025-05-25 23:16:59 +0100

    fuCKING TRANSLATION

--- src/main/java/ellestuff/ellediscs/render/CustomDiscRenderer.java
+++ src/main/java/ellestuff/ellediscs/render/CustomDiscRenderer.java
@@ -24,7 +24,6 @@ import net.minecraft.util.Identifier;
 @Environment(EnvType.CLIENT)
 public class CustomDiscRenderer {
 	public static void renderDisc(ItemStack stack, ModelTransformationMode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-
 		ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
 		var world = MinecraftClient.getInstance().world;
 		var pattern = CustomDiscItem.getPattern(stack);
@@ -35,10 +34,14 @@ public class CustomDiscRenderer {
 		var loadedPatternModel = itemRenderer.getModels().getModelManager().getModel(patternModel);
 
 		matrices.push();
+		matrices.translate(0.5, 0.5, 0.5);
 		loadedBaseModel.getTransformation().getTransformation(mode).apply(leftHanded, matrices);
-		loadedPatternModel.getTransformation().getTransformation(mode).apply(leftHanded, matrices);
+		matrices.scale(-1, 1, -1);
+		matrices.translate(0.03125, 0, 0);
+
 		itemRenderer.renderItem(stack, ModelTransformationMode.NONE, false, matrices, vertexConsumers, light, overlay, loadedBaseModel);
 		itemRenderer.renderItem(stack, ModelTransformationMode.NONE, false, matrices, vertexConsumers, light, overlay, loadedPatternModel);
+
 		matrices.pop();
 	}
 
